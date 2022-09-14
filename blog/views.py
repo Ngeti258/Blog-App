e5a92e98d544dcf5cdf115cb3dec7f2c99530614
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-09-14 12:43:31 +0300

    this where the actual code exists

---
+++ blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+posts=[
+    {
+        'author':'GabrielDN',
+        'title':'Blog Post 1',
+        'content':'First Post Content',
+        'date_posted':'August 27 2018'
+    },
+    {
+        'author':'Jane Doe',
+        'title':'Blog Post 2',
+        'content':'second Post Content',
+        'date_posted':'August 28 2018'
+    }
+]
+
+def Home(request):
+    context={
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
+
+
+def About(request):
+    return render(request,'blog/about.html',{'title':'About'})
+
