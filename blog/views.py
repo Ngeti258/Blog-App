934d1071c153f0f5e259d305b5b5b460e1321832
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-09-16 12:07:44 +0300

    replaced dummy data with actual data created in db

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-posts=[
-    {
-        'author':'GabrielDN',
-        'title':'Blog Post 1',
-        'content':'First Post Content',
-        'date_posted':'August 27 2018'
-    },
-    {
-        'author':'ReginaMW',
-        'title':'Blog Post 2',
-        'content':'second Post Content',
-        'date_posted':'August 28 2018'
-    }
-]
+
 
 def Home(request):
     context={
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
