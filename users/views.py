abd95ffdc474eec1b8b51e5f05eba119b28fe162
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-09-19 11:23:18 +0300

    line 42 was changed to messages instead of message

--- users/views.py
+++ users/views.py
@@ -4,8 +4,8 @@ from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
-         form = UserCreationForm(request.POST)
-         if form.is_valid():
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
             username=form.cleaned_data.get('username')
             messages.success(request,f'Account created for {username}!')
             return redirect('blog-home')
