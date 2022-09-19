eebc1ab2afe3add0a78d6376a86b87ca6129b00f
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-09-19 23:49:40 +0300

    included the profile routeandupdated some features

--- users/views.py
+++ users/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username=form.cleaned_data.get('username')
-            messages.success(request,f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request,f'Your account {username} has been created.You can now login')
+            return redirect('login')
     else:
-         form = UserCreationForm()
+         form = UserRegisterForm()
    
     return render(request,'users/register.html',{'form':form})
 
-
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
 
 
 
