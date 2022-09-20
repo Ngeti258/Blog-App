ad21069bc8acb8764a673c9b8f8e32edf2a69d82
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-09-20 23:22:51 +0300

    no change

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,9 @@
+import email
+import profile
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email=forms.EmailField()
@@ -9,3 +11,14 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model= User
         fields = ['username','email','password1','password2']
+class UserUpdateForm(forms.ModelForm):
+    email=forms.EmailField()
+
+    class Meta:
+        model= User
+        fields = ['username','email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model=profile
+        fields=['image']
--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    image=models.ImageField(default='default.jpg',upload_to='profile_pics')
 
-# Create your models here.
+    def __str__(self):
+        return f'{self.user} Profile'
