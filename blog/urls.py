732dedc6c7e2c3d50ce6b30b48f73c21149a2111
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-09-27 21:43:39 +0300

    added user,updxate and delete route to the urls to make it possible for the user to do listed tasks

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
-from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView
+from .views import PostListView,UserPostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView
 from . import views
 
 
 
 urlpatterns = [
     path('', PostListView.as_view(),name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(),name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(),name='post-detail'),
     path('post/new/', PostCreateView.as_view(),name='post-create'),
-    path('post/<int:pk>/update', PostUpdateView.as_view(),name='post-update'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(),name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(),name='post-delete'),
     path('about/', views.About,name='blog-About'),
    
 ]
