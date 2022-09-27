a7564cbe51f3034c966f1f959e3612e59eaea284
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-09-27 21:44:25 +0300

    switched to class based views and added pagination to the site

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.views.generic import (ListView,
 DetailView,
 UpdateView,
-CreateView)
+CreateView,
+DeleteView)
 from .models import Post
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
 
 
@@ -19,6 +21,18 @@ class PostListView(ListView):
     template_name='blog/home.html'
     context_object_name='posts'
     ordering= ['-date_posted']
+    paginate_by= 3
+
+class UserPostListView(ListView):
+    model = Post
+    template_name='blog/user_posts.html'
+    context_object_name='posts'
+    paginate_by= 3
+
+    def get_queryset(self):
+        user=get_object_or_404(User,username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 
 class PostDetailView(DetailView):
@@ -32,7 +46,7 @@ class PostCreateView(LoginRequiredMixin,CreateView):
         form.instance.author= self.request.user
         return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin,UpdateView):
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     model = Post
     fields=['title','content']
 
@@ -40,6 +54,22 @@ class PostUpdateView(LoginRequiredMixin,UpdateView):
         form.instance.author= self.request.user
         return super().form_valid(form)
     
+    def test_func(self) :
+        post=self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url='/'
+
+    def test_func(self) :
+        post=self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def About(request):
     return render(request,'blog/about.html',{'title':'About'})
